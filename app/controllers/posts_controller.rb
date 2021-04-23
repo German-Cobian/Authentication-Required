5f408dce80e3e7c11939b7aa4699ab82ade7ed3e
German Renato Cobian <germancobian@hotmail.com>
2021-04-23 12:30:18 -0700

    Place before_action in new and create methods of Controller; create form for new.html.erb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,20 @@
 class PostsController < ApplicationController
+# before_action :require_login, only: [:new, :create]
+  
   def index
   end
   
   def new
-    
+    @post = Post.new   
   end
 
   def create
-    
+    @post = Post.new(title: "...", content: "...")
+
+    if @post.save
+      redirect_to @post
+    else
+      render :new
+    end
   end
 end
